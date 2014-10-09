6f80d9f77b02bfe61dfe4a2d4834b5d37a94996e
Jeremy Shipman <jeremy@burnbright.net>
2014-10-09 14:05:01 +1300

    add flex slider back in

---
+++ flexslider/javascript/flexslider.template.js
@@ -0,0 +1,29 @@
+//hacked to make it work?
+
+(function($) {
+
+$(document).ready(function() {
+$('.flexslider_$sliderID').flexslider({
+animation: "$settings_animationType",
+easing: "$settings_easing", // doesnt seem to make a difference
+direction: "$settings_direction",
+reverse: $settings_reverse,
+animationLoop: $settings_animationLoop,
+slideshow: $settings_doSlideshow,
+slideshowSpeed: "$settings_slideshowSpeed",
+animationSpeed: "$settings_animationSpeed",
+randomize: $settings_randomize,
+controlNav: $settings_showControlNav
+//before: FLXslideBeforeHandler
+});
+
+});
+
+FLXslideBeforeHandler = function() {
+//console.log("before!");
+//FLXaddContentToSlides();
+}
+
+//FLXaddContentToSlides = function() {}
+
+})(jQuery);
